7f9e930c596884b349572d9da195923f5cc0cc12
Mikko Markkinen <mikko.markkinen@gmail.com>
2017-05-08 14:52:18 +0300

    solutions

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,56 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[first _] card]
+    (if (Character/isDigit first)
+      (Integer/valueOf (str first))
+      (let [h {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+        (Integer/valueOf(str (get h first)))))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (= (count (set (map suit hand))) 1))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (or
+    (= (sort (vals (frequencies (map rank hand)))) (seq [1 2 2]))
+    (= (sort (vals (frequencies (map rank hand)))) (seq [1 4]))))
 
 (defn straight? [hand]
-  nil)
+  (let [min (apply min (map rank hand))
+        max (apply max (map rank hand))]
+        (if (> min 9)
+          (= (sort (map rank hand)) (range min (+ 1 max)))
+          (if (and (= min 2) (= max 14))
+            (= (sort (replace {14 1} (map rank hand))) (range 1 (+ 1 5)))
+            (= (sort (map rank hand)) (range min (+ 1 max)))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+    (let [checker (fn [x] (if ((first x) hand) (second x) 0))]
+      (apply max (map checker checkers)))))
